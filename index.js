cc65b9f953dc4a4f2921b4145e286280da13b976
Pritam <pritam.sarkar@medlife.com>
2020-02-17 00:34:25 +0530

    Add some calci functions to index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+function plus(a, b) {
+    return a + b;
+}
+
+function minus(a, b) {
+    return a - b;
+}
+
+function diff(a, b) {
+    return a > b ? (a - b) : (b - a);
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
